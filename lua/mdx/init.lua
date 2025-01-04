0ab6be56b5a5dbf6a871fa9a871a8b7d8a7a8ff0
David Mejorado <david.mejorado@gmail.com>
2025-01-04 10:59:21 -0800

    fix: ensure the filetype is set on lazy load

--- lua/mdx/init.lua
+++ lua/mdx/init.lua
@@ -6,6 +6,11 @@ function M.setup()
 
     -- Configure treesitter to use the markdown parser for mdx files
     vim.treesitter.language.register("markdown", "mdx")
+
+    -- If the current buffer has the extension mdx, but not the newly create filetype, set it
+    if vim.endswith(vim.api.nvim_buf_get_name(0), ".mdx") and vim.o.filetype ~= "mdx" then
+        vim.o.filetype = "mdx"
+    end
 end
 
 return M
