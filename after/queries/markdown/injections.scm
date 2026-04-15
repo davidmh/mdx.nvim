7157f975814c7602e51ede5ad96cc048b31b17ad
David Mejorado <david.mejorado@gmail.com>
2026-04-14 18:00:04 -0700

    chore: multi-line support on indented components
    Supersedes and closes #11
    
    Co-authored-by: Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>

--- after/queries/markdown/injections.scm
+++ after/queries/markdown/injections.scm
@@ -6,8 +6,16 @@
 
 ((inline) @injection.content
   (#lua-match? @injection.content "^%s*export")
-  (#set! injection.language "typescript"))
+  (#set! injection.language "typescriptreact"))
 
 ((inline) @injection.content
   (#lua-match? @injection.content "^<")
   (#set! injection.language "typescriptreact"))
+
+; Multi-line JSX component bodies: when a component spans multiple lines,
+; markdown parses the indented body as an indented_code_block. Inject TSX
+; so it gets proper syntax highlighting.
+((indented_code_block) @injection.content
+  (#lua-match? @injection.content "^%s*<")
+  (#set! injection.language "typescriptreact")
+  (#set! injection.include-children))
